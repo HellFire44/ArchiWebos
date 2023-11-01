8b35a59125be54859dfccc1fc2138d8114c3ac17
Emilien Griffon <emiliengriffon@hotmail.fr>
2023-11-01 22:00:31 +0100

    Update

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,1 +1,14 @@
-// Modal
+export function DataModal() {
+
+// Récupérez l'élément modal avec l'ID "modal"
+const modalDialog = document.getElementById("modal");
+
+// Créez un élément h1 avec le texte "test"
+const title = document.createElement('h2');
+title.textContent = "Galerie photo";
+
+// Ajoutez l'élément title au modal
+modalDialog.appendChild(title);
+
+
+}
--- FrontEnd/setupGallery.js
+++ FrontEnd/setupGallery.js
@@ -1,23 +1,26 @@
+import { DataModal } from "./modal.js";
+
+
 export function setupGallery() {
 
-    const gallery = document.querySelector(".gallery");
-    const filterContainer = document.querySelector(".container-filter");
-    const allFilter = document.getElementById("all-filter");
+  const gallery = document.querySelector(".gallery");
+  const filterContainer = document.querySelector(".container-filter");
+  const allFilter = document.getElementById("all-filter");
   
-
-    function showImagesByCategory(categoryId) {
+  
+  function showImagesByCategory(categoryId) {
       gallery.innerHTML = "";
       fetch("http://localhost:5678/api/works") // API IMAGES
-        .then(response => response.json())
-        .then(images => {
-          images.forEach(imageData => {
-            if (categoryId === null || imageData.category.id === categoryId) {
+      .then(response => response.json())
+      .then(images => {
+        images.forEach(imageData => {
+          if (categoryId === null || imageData.category.id === categoryId) {
               const figure = document.createElement("figure");
               const img = document.createElement("img");
               img.src = imageData.imageUrl;
               const figcaption = document.createElement("figcaption");
               figcaption.textContent = imageData.title;
-  
+              
               figure.appendChild(img);
               figure.appendChild(figcaption);
               gallery.appendChild(figure);
@@ -27,46 +30,57 @@ export function setupGallery() {
         .catch(error => {
           console.error("Erreur lors du chargement des images : ", error);
         });
-    }
-
-function getAuthToken() {
-  return localStorage.getItem("authToken");
-}
-
-if (getAuthToken(true)) {
+      }
+      
+      function getAuthToken() {
+        return localStorage.getItem("authToken");
+      }
+      
+      if (getAuthToken(true)) {
         showImagesByCategory(null);
         filterContainer.innerHTML = ""; 
-
+        
         const portfolioSection = document.getElementById("portfolio");
-
+        
         const editButton = document.createElement('button');
-
+        
         const icon = document.createElement('i');
         icon.classList.add("fa-regular", "fa-pen-to-square");
-
+        
         const buttonText = document.createTextNode("Modifier");
-
+        
         editButton.appendChild(icon);
         editButton.appendChild(buttonText);
-
+        
         editButton.classList.add("edit-btn");
-
+        
         const titleElement = portfolioSection.querySelector("h2");
         titleElement.appendChild(editButton);
 
+
+        const modal = document.querySelector("#modal")
+        const closeBtn = document.querySelector("#closemodal")
+        //  MODAL OUVERT 
         editButton.addEventListener("click", function () {
 
-          //  Si un click et fait sur bouton alors il faut afficher le modal
+          modal.showModal()
+          DataModal()
+              });
+
+        // MODAL FERMER
+        closeBtn.addEventListener("click", function () {
+
+          modal.close()
         });
-
+        
         const logout = document.getElementById("logout");
-
+        
         logout.innerHTML = "logout";
-
+        
         logout.addEventListener("click", function() {
           localStorage.clear("authToken")
         } )
-} else {
+      } else {
   console.log("Compte Non-Connecté")
   fetch("http://localhost:5678/api/categories") // API CATEGORIES
   .then(response => response.json())
@@ -90,13 +104,14 @@ if (getAuthToken(true)) {
   .catch(error => {
     console.error("Erreur lors du chargement des catégories depuis l'API : ", error);
   });
-
+  
     allFilter.addEventListener("click", function() {
       showImagesByCategory(null);
     });
-  
+    
     showImagesByCategory(null);
- }
+  }
+
 
   }
   
